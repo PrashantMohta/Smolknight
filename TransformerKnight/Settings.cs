7021933c7f567988d8d0645069466ee86fa503cc
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2021-07-03 00:43:00 +0400

    Add the files needed for new update

---
+++ TransformerKnight/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InControl;
+using Modding.Converters;
+using Newtonsoft.Json;
+
+namespace TransformerKnight
+{
+    public class Settings
+    {
+        [JsonConverter(typeof(PlayerActionSetConverter))]
+        public KeyBinds keybinds = new KeyBinds();
+    }
+    public class KeyBinds : PlayerActionSet
+    {
+        public PlayerAction Transform;
+
+        public KeyBinds()
+        {
+            Transform = CreatePlayerAction("Transform");
+            DefaultBinds();
+        }
+
+        private void DefaultBinds()
+        {
+            Transform.AddDefaultBinding(Key.Backspace);
+        }
+    }
+}
