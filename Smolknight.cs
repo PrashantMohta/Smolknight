08fc36e4a64813fbac9b1f99cc5570b348b0cbf5
Prashant Mohta <mohtaprashantrocks@gmail.com>
2022-01-30 23:18:54 +0530

    adds shaman NPC structure

--- Smolknight.cs
+++ Smolknight.cs
@@ -25,6 +25,10 @@ namespace SmolKnight
         public static SmolKnight Instance;
         public static GameObject KnightControllerGo;
         public static KnightController knightController;
+        public static Satchel.Core satchel = new Satchel.Core();
+        public Dictionary<string, Dictionary<string, GameObject>> preloads;
+
+        public static Shaman shaman;
         public static float currentScale = Size.SMOL;
         public static float GetCurrentScale(){
             return currentScale;
@@ -98,35 +102,63 @@ namespace SmolKnight
             return ModMenu.CreatemenuScreen();
         }
 
-        public override void Initialize()
+        public override List<(string, string)> GetPreloadNames()
         {
-            Instance = this;
+            return new List<(string, string)>
+            {
+                ("Cliffs_01","Cornifer Card")
+            };
+        }
+        public CustomDialogueManager customDialogueManager;
+        public GameObject CardPrefab;
+        private void CreateCustomDialogueManager(){
+            if(customDialogueManager == null){
+                customDialogueManager = satchel.GetCustomDialogueManager(CardPrefab);
+            }
+        }
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            if(preloadedObjects != null){
 
-            IL.HeroController.Update10 += ILHooks.BypassCheckForKnightScaleRange;
-            ILHooks.InitCustomHooks();
+                preloads = preloadedObjects;
+                Instance = this;
 
-            ModHooks.HeroUpdateHook += HeroUpdate;
-            ModHooks.BeforePlayerDeadHook += Shade.OnHeroDeath;
-            ModHooks.AfterSavegameLoadHook += LoadSaveGame;
 
-            ModHooks.SetPlayerFloatHook += PlayerDataPatcher.SetPlayerFloat;
-            ModHooks.GetPlayerFloatHook += PlayerDataPatcher.GetPlayerFloat;
 
-            On.HeroController.FaceLeft += HeroControllerPatcher.FaceLeft;
-            On.HeroController.FaceRight += HeroControllerPatcher.FaceRight;
-            On.HeroController.FindGroundPointY += HeroControllerPatcher.FindGroundPointY;
-            On.HeroController.FindGroundPoint += HeroControllerPatcher.FindGroundPoint;
-            
-            On.HeroController.EnterScene += GatePatcher.EnterScene;
-            On.HeroController.FinishedEnteringScene += GatePatcher.FinishedEnteringScene;
-            
-            On.HutongGames.PlayMaker.Actions.SpawnObjectFromGlobalPool.OnEnter += ActionPatcher.OnSpellSpawn;
-            On.HutongGames.PlayMaker.Actions.SetScale.DoSetScale += ActionPatcher.DoSetScale;
-            On.HutongGames.PlayMaker.Actions.RayCast2d.OnEnter += ActionPatcher.OnRayCast2d;
-            On.HutongGames.PlayMaker.Actions.CreateObject.OnEnter += ActionPatcher.CreateObject;
+                IL.HeroController.Update10 += ILHooks.BypassCheckForKnightScaleRange;
+                ILHooks.InitCustomHooks();
 
-            On.UIManager.HideCurrentMenu += HideCurrentMenu;
-            UnityEngine.SceneManagement.SceneManager.sceneLoaded += ShinyItemStandPatcher.StartPatchCoro;
+                ModHooks.HeroUpdateHook += HeroUpdate;
+                ModHooks.BeforePlayerDeadHook += Shade.OnHeroDeath;
+                ModHooks.AfterSavegameLoadHook += LoadSaveGame;
+
+                ModHooks.SetPlayerFloatHook += PlayerDataPatcher.SetPlayerFloat;
+                ModHooks.GetPlayerFloatHook += PlayerDataPatcher.GetPlayerFloat;
+
+                On.HeroController.FaceLeft += HeroControllerPatcher.FaceLeft;
+                On.HeroController.FaceRight += HeroControllerPatcher.FaceRight;
+                On.HeroController.FindGroundPointY += HeroControllerPatcher.FindGroundPointY;
+                On.HeroController.FindGroundPoint += HeroControllerPatcher.FindGroundPoint;
+                
+                On.HeroController.EnterScene += GatePatcher.EnterScene;
+                On.HeroController.FinishedEnteringScene += GatePatcher.FinishedEnteringScene;
+                
+                On.HutongGames.PlayMaker.Actions.SpawnObjectFromGlobalPool.OnEnter += ActionPatcher.OnSpellSpawn;
+                On.HutongGames.PlayMaker.Actions.SetScale.DoSetScale += ActionPatcher.DoSetScale;
+                On.HutongGames.PlayMaker.Actions.RayCast2d.OnEnter += ActionPatcher.OnRayCast2d;
+                On.HutongGames.PlayMaker.Actions.CreateObject.OnEnter += ActionPatcher.CreateObject;
+
+                On.UIManager.HideCurrentMenu += HideCurrentMenu;
+                UnityEngine.SceneManagement.SceneManager.sceneLoaded += ShinyItemStandPatcher.StartPatchCoro;
+
+                CardPrefab = preloads["Cliffs_01"]["Cornifer Card"];
+                CreateCustomDialogueManager();
+                CustomArrowPrompt.Prepare(CardPrefab);
+                shaman = new Shaman();
+                shaman.AddCustomDialogue(customDialogueManager);
+
+            }
+
         }        
 
         //warpToDreamGate
